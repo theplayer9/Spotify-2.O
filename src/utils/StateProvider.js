6837a02d82820dc1b72b91763845e4f4aff19a1f
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-03 00:54:20 +0530

    Created the context file

---
+++ src/utils/StateProvider.js
@@ -0,0 +1,9 @@
+import { createContext, useContext, useReducer } from "react";
+
+const StateContext = createContext();
+
+const StateProvider =({children, initialState, reducer})=>(
+   <StateContext.Provider value={useReducer(reducer,initialState)}>
+    {children}
+   </StateContext.Provider>
+);
