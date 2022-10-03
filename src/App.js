dee7baee3edabdb4641d34ba8eed7a73e4fe206a
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-04 00:25:28 +0530

    hash extracted

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ const App = () => {
 
   useEffect(()=>{
     const hash = window.location.hash;
-    console.log(hash)
-  })
+    // console.log(hash)
+    if(hash){
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      console.log("This is :",token);
+    }
+  },[])
   return (
     <Login />
   )
