b6ee84a6936dd0c06fd6beeb9c48746a6ce31e38
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-04 00:00:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './components/Login'
 
 const App = () => {
+
+  useEffect(()=>{
+    const hash = window.location.hash;
+    console.log(hash)
+  })
   return (
     <Login />
   )
