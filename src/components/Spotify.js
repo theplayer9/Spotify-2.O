d3a629f5cc40c082b364f74b3ccae2ef12308bc1
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-05 02:23:52 +0530

    Structure for the page

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Body from './Body'
 
 const Spotify = () => {
   return (
-    <div>Spotify</div>
+    <Container>
+      <div className='spotify__body'>
+        <Sidebar />
+        <div className='body' >
+          <Navbar />
+          <div className='body__contents' >
+            <Body />
+          </div>
+        </div>
+      </div>
+      <div className='spotify__footer' ><Footer /></div>
+    </Container>
   )
 }
 
-export default Spotify
+export default Spotify;
+
+
+
+const Container = styled.div``;
