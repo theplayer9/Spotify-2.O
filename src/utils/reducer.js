a32076ab0a298b4a68cf1678df871b2aacc900c0
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-05 01:02:12 +0530

    added the reducer logic

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { reducerCases } from './Constants';
 
 const reducer = (state, action) => {
     switch (action.type) {
+        case reducerCases.SET_TOKEN:{
+            return {
+                ...state,
+                token: action.token,
+            }
+        }
         default: return state;
     }
 }
