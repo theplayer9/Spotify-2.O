d413dd9a97f5cd9b656ea4365e1085697009dde3
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-05 22:59:17 +0530

    styled the playlist

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -42,4 +42,32 @@ export default Playlists
 
 
 const Container = styled.div` 
+height: 100%;
+overflow: hidden;
+ul{
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 45vh;
+    max-height: 100%;
+    overflow: auto;
+    &::webkit-scrollbar{
+       width: 0.7 rem;
+       &-thumb {
+        background-color: rgba(255,255,255,0.6);
+       }
+    }
+    li {
+      display: flex;
+      gap: 1rem;
+      cursor: pointer;
+      transition: 0.4s ease-in-out;
+      &:hover {
+        color: white;
+      }
+    }
+
+  }
 `;
