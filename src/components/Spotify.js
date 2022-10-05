1c6a4f50c5ab931f1cf4f76e65b65377c87d799c
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-05 16:10:01 +0530

    styling the spotify__body

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -36,6 +36,17 @@ grid-template-rows: 85vh 15vh;
 .spotify__body {
   display: grid;
   grid-template-columns: 15vw 85vw;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(transparent , rgba(0,0,0,1));
+  background-color: rgb(32,87,100);
+}
+
+.body {
+  height: 100%;
+  width:100%;
+  overflow: auto;
+  
 }
 
 `;
