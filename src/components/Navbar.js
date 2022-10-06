a4a1a0a7490111007cc9b3c063171e7153cd59e7
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-06 13:16:34 +0530

    some changes in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+import {FaSearch} from 'react-icons/fa'
+import {CgProfile} from "react-icons/cg"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+      <div className='search__bar'>
+        <FaSearch />
+        <input type="text" placeholder="Artists, songs, or podcasts" />
+      </div>
+      <div className='avatar'>
+        <a href='#'>
+          <CgProfile />
+          <span>{userInfo?.name}</span>
+        </a>
+      </div>
+    </Container>
   )
 }
 
-export default Navbar
+export default Navbar;
+
+
+const Container= styled.div``;
