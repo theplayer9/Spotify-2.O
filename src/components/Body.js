c5d6ce560dad10cd88ca3954062c12dbb0357410
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-09 01:58:17 +0530

    fetching info of individual album

--- src/components/Body.js
+++ src/components/Body.js
@@ -88,7 +88,19 @@ const Body = () => {
                         <span>{index + 1}</span>
                       </div>
                       <div className='col detail'>
-                        <img src={image} alt="track" />
+                        <div className='image'>
+                          <img src={image} alt="track" />
+                        </div>
+                        <div className='info'>
+                          <span className='name'>{name}</span>
+                          <span>{artists}</span>
+                        </div>
+                      </div>
+                      <div className='col'>
+                        <span> {album}</span>
+                      </div>
+                      <div className='col'>
+                        <span>{duration}</span>
                       </div>
                     </div>
                   )
