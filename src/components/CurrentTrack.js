67236057dee9578274fcae68da8b9a3e72068ad7
Om Sharma <omsharma.theplayer11@gmail.com>
2022-12-26 23:36:20 +0530

    nothing

--- src/components/CurrentTrack.js
+++ src/components/CurrentTrack.js
@@ -56,7 +56,7 @@ width: 100%;
 border-top: 1px solid #282828;
 display: grid;
 grid-template-columns: 1fr 2fr 1fr;
-// align-items: center;
+align-items: center;
 justify-content: center;
 padding: 0 1rem;
 `;
